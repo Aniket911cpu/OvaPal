1ba3aacbaf6022e507e23b3ec8fd038b5caa22a3
MasterAniket <aniketmaji265@gmail.com>
2023-06-12 20:09:50 +0530

    Logo Changed & Splash Screen Added

---
+++ lib/Screens/medicines.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class MedicinesPage extends StatefulWidget {
+  const MedicinesPage({super.key});
+
+  @override
+  State<MedicinesPage> createState() => _MedicinesPageState();
+}
+
+class _MedicinesPageState extends State<MedicinesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(),
+    );
+  }
+}
